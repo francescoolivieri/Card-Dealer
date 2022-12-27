31ece1cfb775497ef2658ea253dd4d7e3803b653
francescoolivieri <francescoolivieri45@gmail.com>
2022-12-27 22:34:06 +0100

    People Detection V1

--- CardDealer/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/Francesco\ Olivieri/Documents/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
-
